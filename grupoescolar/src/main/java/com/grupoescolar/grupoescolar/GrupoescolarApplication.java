19925db3b9dc59f59684b8e767c89bcdfe8da5eb
Patricia A. da Silva <patriciaananias1983@hotmail.com>
2023-05-30 19:12:29 -0300

    Add files via upload

---
+++ grupoescolar/src/main/java/com/grupoescolar/grupoescolar/GrupoescolarApplication.java
@@ -0,0 +1,13 @@
+package com.grupoescolar.grupoescolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoescolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupoescolarApplication.class, args);
+	}
+
+}
